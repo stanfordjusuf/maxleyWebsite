79c5dfb00ad258f8f6b9dff1def6da234b442d24
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-16 16:00:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "../styles/globals.scss";
-import { Layout } from "../components";
+import React from 'react';
+import '../styles/globals.scss';
+import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <br></br>
       <Layout>
         <Component {...pageProps} />
       </Layout>
