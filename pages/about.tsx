b373f9a01381bce2151a51e66fd6c49b4efe5873
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-03-16 16:02:43 +0700

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 
 export default function about() {
   return (
     <div className="grid md:grid-cols-1 lg:grid-cols-2 h-screen">
       <div className="bg-black">
         <img
-          src={"https://i.ibb.co/3CcL9SB/Living-Room.jpg"}
+          src='https://i.ibb.co/3CcL9SB/Living-Room.jpg'
           className="h-full"
-        ></img>
+        />
       </div>
       <div className="flex-1 p-4">
         <div className="border-b-2 border-b-[#f86c04]">
           <h1 className="text-4xl font-bold ">Our Story</h1>
         </div>
 
-        <br></br>
+        <br/>
         <p className="text-md shrink min-w-min">
           Maxley adalah sebuah Student Dormitory yang berdiri sejak tahun 2015,
           terletak di Edutown, seberang Universitas Prasetya Mulya, sudah
@@ -26,12 +26,11 @@ export default function about() {
           menerima tamu sewa seperti Karyawan/ Staff dan Daily Rent sesuai
           dengan ketentuan yang berlaku.
         </p>
-        <br></br>
+        <br/>
         <a
           href="https://www.tiktok.com/@maxleysuites?_t=8aRWNgJ7DDm&_r=1"
-          target="_blank"
           className="btn bg-[#f86c04]"
-          style={{ color: "black" }}
+          style={{ color: 'black' }}
         >
           Check out our Tiktok
         </a>
